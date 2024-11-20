590f5fca486b04f49e2ebbaf8f86e7619970b5a2
SourC <souriya.khaosanga@gmail.com>
2024-11-19 19:57:30 -0800

    Refactor test workflow and update dependencies

--- __tests__/progress-reporter.ts
+++ __tests__/progress-reporter.ts
@@ -38,19 +38,19 @@ export default class ProgressReporter implements Reporter {
 
   onTestFailed(test: TaskResult) {
     this.failed++;
-    const fileResults = this.getFileResults(test.file?.name || 'unknown');
+    const fileResults = this.getFileResults(test.taskFile?.name || 'unknown');
     fileResults.failed++;
     
     this.errors.push({
-      file: test.file?.name || 'unknown',
-      test: test.name,
-      error: test.error || new Error('Unknown error')
+      file: test.taskFile?.name || 'unknown',
+      test: test.taskName || 'unknown',
+      error: test.errors?.[0] || new Error('Unknown error')
     });
   }
 
   onTestPassed(test: TaskResult) {
     this.passed++;
-    const fileResults = this.getFileResults(test.file?.name || 'unknown');
+    const fileResults = this.getFileResults(test.taskFile?.name || 'unknown');
     fileResults.passed++;
   }
 
