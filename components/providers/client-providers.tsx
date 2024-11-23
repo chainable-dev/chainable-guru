8be3073abed255205d31cf56ce2b91368bfa11b2
SourC <souriya.khaosanga@gmail.com>
2024-11-22 18:25:07 -0800

    refactor: Add create-chainable-icon script and update preview-attachment component

--- components/providers/client-providers.tsx
+++ components/providers/client-providers.tsx
@@ -4,47 +4,37 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { RainbowKitProvider, darkTheme, lightTheme } from '@rainbow-me/rainbowkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
+import { ThemeProvider } from 'next-themes';
 import Disclaimer from '@/components/custom/disclaimer';
-
 import { config } from '@/lib/wallet/config';
-import { useTheme } from 'next-themes';
 
 const queryClient = new QueryClient();
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
-  const { theme } = useTheme();
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          appInfo={{
-            appName: 'ElronAI',
-            disclaimer: Disclaimer,
-          }}
-          theme={theme === 'dark' ? darkTheme({
-            accentColor: '#BD93F9', // Dracula purple
-            accentColorForeground: '#F8F8F2', // Dracula foreground
-            fontStack: 'system',
-            overlayBlur: 'small',
-            borderRadius: 'medium',
-          
-          }) : lightTheme({
-            accentColor: '#BD93F9', // Dracula purple
-            accentColorForeground: '#282A36', // Dracula background
-            fontStack: 'system', 
-            overlayBlur: 'small',
-            borderRadius: 'medium',
-          })
-
-        }
-          modalSize="compact"
-          coolMode
-          showRecentTransactions={true}
-          
-        >
-          {children}
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider
+            theme={darkTheme({
+              accentColor: '#BD93F9',
+              accentColorForeground: '#F8F8F2',
+              fontStack: 'system',
+              overlayBlur: 'small',
+              borderRadius: 'medium',
+            })}
+            appInfo={{
+              appName: 'ElronAI',
+              disclaimer: Disclaimer,
+            }}
+            modalSize="compact"
+            coolMode
+            showRecentTransactions={true}
+          >
+            {children}
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </ThemeProvider>
   );
 } 
