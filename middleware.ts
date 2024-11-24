756664f5aa5212a354cda785d0d5ff200225688f
SourC <souriya.khaosanga@gmail.com>
2024-11-24 12:19:24 -0800

    feat: Replace custom middleware with Clerk's auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,9 @@
-import { NextRequest } from "next/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-import { updateSession } from "@/lib/supabase/middleware";
-
-export async function middleware(request: NextRequest) {
-	return await updateSession(request);
-}
+export default authMiddleware({
+	publicRoutes: ["/", "/api/webhook", "/api/search"],
+});
 
 export const config = {
-	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico (favicon file)
-		 * Feel free to modify this pattern to include more paths.
-		 */
-		"/((?!_next/static|_next/image|favicon.ico).*)",
-	],
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
